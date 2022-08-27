8ed77b348889da18ba4abb95758e3c2fed9f7565
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-08-27 15:20:25 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Kike Hernandez D. on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
