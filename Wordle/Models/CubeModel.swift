0d81cd4375e8ad7a7dff584e231b453ad6a10367
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-08-29 17:51:42 -0500

    Make cube model

---
+++ Wordle/Models/CubeModel.swift
@@ -0,0 +1,39 @@
+//
+//  CubeModel.swift
+//  Wordle
+//
+//  Created by Kike Hernandez D. on 29/08/22.
+//
+
+import SwiftUI
+
+enum Status {
+    case normal
+    case match
+    case dontAppear
+    case apear
+}
+
+struct CubeModel: Hashable {
+    let letter: String
+    var status: Status
+    
+    var color: Color {
+        switch status {
+        case .normal:
+            return Color.white
+        case .match:
+            return Color.green
+        case .dontAppear:
+            return Color.gray
+        case .apear:
+            return Color.yellow
+        }
+    }
+    
+    init(_letter: String) {
+        self.letter = _letter
+        self.status = .normal
+    }
+}
+
